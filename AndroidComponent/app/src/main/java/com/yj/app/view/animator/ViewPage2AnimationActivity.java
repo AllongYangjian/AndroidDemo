5463578054e59d7748a76b571a5094f42230a58b
AllongYangjian <952801974@qq.com>
2020-10-22 21:09:20 +0800

    viewpager切换动画

--- AndroidComponent/app/src/main/java/com/yj/app/view/animator/ViewPage2AnimationActivity.java
+++ AndroidComponent/app/src/main/java/com/yj/app/view/animator/ViewPage2AnimationActivity.java
@@ -1,13 +1,23 @@
 package com.yj.app.view.animator;
 
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.yj.app.R;
 import com.yj.app.databinding.ActivityViewPage2Binding;
 import com.yj.app.databinding.ActivityViewPageBinding;
+import com.yj.app.view.fragment.ScreenSlidePageFragment;
 
 import java.util.Objects;
 
@@ -17,17 +27,83 @@ import java.util.Objects;
 public class ViewPage2AnimationActivity extends AppCompatActivity {
 
     private ActivityViewPage2Binding binding;
+    private final int MAX_NUM = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_view_page2);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_view_page2);
         setSupportActionBar(binding.toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         initView();
     }
 
-    private void initView(){
-
+    private void initView() {
+        MyPagerAdapter adapter = new MyPagerAdapter(this);
+        binding.viewPager.setAdapter(adapter);
+        binding.viewPager.setPageTransformer(new DepthPageTransformer());
     }
+
+    @Override
+    public void onBackPressed() {
+        if (binding.viewPager.getCurrentItem() == 0)
+            super.onBackPressed();
+        else
+            binding.viewPager.setCurrentItem(binding.viewPager.getCurrentItem() - 1);
+    }
+
+    private class MyPagerAdapter extends FragmentStateAdapter {
+
+        public MyPagerAdapter(@NonNull FragmentActivity activity) {
+            super(activity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return new ScreenSlidePageFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return MAX_NUM;
+        }
+    }
+    public static class DepthPageTransformer implements ViewPager2.PageTransformer {
+        private static final float MIN_SCALE = 0.75f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0f);
+
+            } else if (position <= 0) { // [-1,0]
+                // Use the default slide transition when moving to the left page
+                view.setAlpha(1f);
+                view.setTranslationX(0f);
+                view.setScaleX(1f);
+                view.setScaleY(1f);
+
+            } else if (position <= 1) { // (0,1]
+                // Fade the page out.
+                view.setAlpha(1 - position);
+
+                // Counteract the default slide transition
+                view.setTranslationX(pageWidth * -position);
+
+                // Scale the page down (between MIN_SCALE and 1)
+                float scaleFactor = MIN_SCALE
+                        + (1 - MIN_SCALE) * (1 - Math.abs(position));
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0f);
+            }
+        }
+    }
+
 }
--- AndroidComponent/app/src/main/java/com/yj/app/view/animator/ViewPageAnimationActivity.java
+++ AndroidComponent/app/src/main/java/com/yj/app/view/animator/ViewPageAnimationActivity.java
@@ -1,13 +1,19 @@
 package com.yj.app.view.animator;
 
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.yj.app.R;
-import com.yj.app.databinding.ActivityTemplateBinding;
 import com.yj.app.databinding.ActivityViewPageBinding;
+import com.yj.app.view.fragment.ScreenSlidePageFragment;
 
 import java.util.Objects;
 
@@ -18,16 +24,117 @@ public class ViewPageAnimationActivity extends AppCompatActivity {
 
     private ActivityViewPageBinding binding;
 
+    private final int MAX_NUMS = 5;
+    private ScreenSlidPagerAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_view_page);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_view_page);
         setSupportActionBar(binding.toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         initView();
     }
 
-    private void initView(){
-
+    private void initView() {
+        adapter = new ScreenSlidPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        binding.viewPager.setAdapter(adapter);
+        binding.viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
     }
+
+    @Override
+    public void onBackPressed() {
+        if (binding.viewPager.getCurrentItem() == 0) {
+            super.onBackPressed();
+        } else {
+            binding.viewPager.setCurrentItem(binding.viewPager.getCurrentItem() - 1);
+        }
+    }
+
+    private class ScreenSlidPagerAdapter extends FragmentStatePagerAdapter {
+
+        public ScreenSlidPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return new ScreenSlidePageFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return MAX_NUMS;
+        }
+    }
+
+    private class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+
+        private static final float MIN_SCALE = 0.85F;
+        private static final float MIN_ALPAH = 0.5f;
+
+        @Override
+        public void transformPage(@NonNull View page, float position) {
+            int pageWidth = page.getWidth();
+            int pageHeight = page.getHeight();
+            if (position < -1) {
+                page.setAlpha(0f);
+            } else if (position <= 1) {
+                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                float verMargin = pageHeight * (1 - scaleFactor) / 2;
+                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                if (position < 0) {
+                    page.setTranslationX(horzMargin - verMargin / 2);
+                } else {
+                    page.setTranslationX(-horzMargin + verMargin / 2);
+                }
+
+                page.setScaleX(scaleFactor);
+                page.setScaleY(scaleFactor);
+
+                page.setAlpha(MIN_ALPAH + (scaleFactor - MIN_SCALE) / (1 - MIN_SCALE) * (1 - MIN_ALPAH));
+            } else {
+                page.setAlpha(0f);
+            }
+        }
+    }
+
+    public class DepthPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.75f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0f);
+
+            } else if (position <= 0) { // [-1,0]
+                // Use the default slide transition when moving to the left page
+                view.setAlpha(1f);
+                view.setTranslationX(0f);
+                view.setScaleX(1f);
+                view.setScaleY(1f);
+
+            } else if (position <= 1) { // (0,1]
+                // Fade the page out.
+                view.setAlpha(1 - position);
+
+                // Counteract the default slide transition
+                view.setTranslationX(pageWidth * -position);
+
+                // Scale the page down (between MIN_SCALE and 1)
+                float scaleFactor = MIN_SCALE
+                        + (1 - MIN_SCALE) * (1 - Math.abs(position));
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0f);
+            }
+        }
+    }
+
 }
